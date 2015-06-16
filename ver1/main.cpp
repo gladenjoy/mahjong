e922c793a66460afc527ae2743e86a4d897f280c
gladenjoy <gladenjoy.is01@gmail.com>
2015-06-16 20:09:03 +0900

    Initial Commit

---
+++ ver1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ver1
+//
+//  Created by 喜楽 智規 on 2015/06/16.
+//  Copyright (c) 2015年 Kiraku Tomoki. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
